ca2d4599f97d5102f1a1f104672108e7c4ffe7a0
Anshi Patel <anshipatel881@gmail.com>
2025-01-30 22:30:38 +0530

    HashMap implementation

---
+++ Day_02/src/HashMapAndHashSet/HashMapImplementation/CustomHashMap.java
@@ -0,0 +1,92 @@
+package HashMapAndHashSet.HashMapImplementation;
+
+public class CustomHashMap<K,V>{
+    private static class Entry<K, V> {
+        K key;
+        V value;
+        Entry<K, V> next;
+
+        Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private static final int SIZE = 16;
+    private Entry<K, V>[] table;
+
+    @SuppressWarnings("unchecked")
+    public CustomHashMap() {
+        table = new Entry[SIZE];
+    }
+
+    private int getIndex(K key) {
+        return key.hashCode() % SIZE;
+    }
+
+    public void put(K key, V value) {
+        int index = getIndex(key);
+        Entry<K, V> newEntry = new Entry<>(key, value);
+
+        if (table[index] == null) {
+            table[index] = newEntry;
+        } else {
+            Entry<K, V> current = table[index];
+            while (current.next != null) {
+                if (current.key.equals(key)) {
+                    current.value = value;
+                    return;
+                }
+                current = current.next;
+            }
+            if (current.key.equals(key)) {
+                current.value = value;
+            } else {
+                current.next = newEntry;
+            }
+        }
+    }
+
+    public V get(K key) {
+        int index = getIndex(key);
+        Entry<K, V> current = table[index];
+
+        while (current != null) {
+            if (current.key.equals(key)) {
+                return current.value;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
+    public void remove(K key) {
+        int index = getIndex(key);
+        Entry<K, V> current = table[index];
+        Entry<K, V> prev = null;
+
+        while (current != null) {
+            if (current.key.equals(key)) {
+                if (prev == null) {
+                    table[index] = current.next;
+                } else {
+                    prev.next = current.next;
+                }
+                return;
+            }
+            prev = current;
+            current = current.next;
+        }
+    }
+    public static void main(String[] args) {
+        CustomHashMap<String, Integer> map = new CustomHashMap<>();
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+
+        System.out.println("Value for 'two': " + map.get("two"));
+        map.remove("two");
+        System.out.println("Value for 'two' after removal: " + map.get("two"));
+    }
+}
